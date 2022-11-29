3ef446ee3077ea64703e1773d468d175ded3c566
yann <yann@saynode.ch>
2022-11-29 14:15:29 +0100

    home not test page

--- tezos_swap_frontend/lib/main.dart
+++ tezos_swap_frontend/lib/main.dart
@@ -50,32 +50,29 @@ class MyApp extends StatelessWidget {
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
+      home: FutureBuilder<List<dynamic>>(
+        future: Future.wait([tokenRepo.init(), contractRepo.init()]),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If we got an error
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  '${snapshot.error} occurred',
+                  style: const TextStyle(fontSize: 18),
+                ),
+              );
 
- home: TestPage()
-
-      // FutureBuilder<List<dynamic>>(
-      //   future: Future.wait([tokenRepo.init(), contractRepo.init()]),
-      //   builder: (ctx, snapshot) {
-      //     if (snapshot.connectionState == ConnectionState.done) {
-      //       // If we got an error
-      //       if (snapshot.hasError) {
-      //         return Center(
-      //           child: Text(
-      //             '${snapshot.error} occurred',
-      //             style: const TextStyle(fontSize: 18),
-      //           ),
-      //         );
-
-      //         // if we got our data
-      //       } else if (snapshot.hasData) {
-      //         return const HomePage();
-      //       }
-      //     }
-      //     return const Center(
-      //       child: Text('Fatal Error, this should never happen.'),
-      //     );
-      //   },
-      // ),
+              // if we got our data
+            } else if (snapshot.hasData) {
+              return const HomePage();
+            }
+          }
+          return const Center(
+            child: Text('Fatal Error, this should never happen.'),
+          );
+        },
+      ),
     );
   }
 }
