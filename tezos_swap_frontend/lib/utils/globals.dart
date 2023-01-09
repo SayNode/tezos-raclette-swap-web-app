3524a74538c30d58eca656f6ea7031841b6c23b4
yann <yann@saynode.ch>
2023-01-09 15:24:39 +0100

    updated to new address

--- tezos_swap_frontend/lib/utils/globals.dart
+++ tezos_swap_frontend/lib/utils/globals.dart
@@ -3,7 +3,7 @@ import 'package:tezos_swap_frontend/models/contract_model.dart';
 import '../services/wallet_connection.dart';
 
 final WalletProvider walletProvider = WalletProvider();
-const String testContract = 'KT1HVFJZhSny2GbikHpZuWkfgrZd3vRbFeaP';
+const String testContract = 'KT1TZTkhnZFPL7cdNaif9B3r5oQswM1pnCXB';
 const String networkUri = 'https://ghostnet.tezos.marigold.dev';
 const String network = 'ghostnet';
 List<Contract>? contracts;
