549c5c5e9de9251a11cede0b9ab08963bb8b643a
João Gonçalo Morais <71322340+JoaoMorais96@users.noreply.github.com>
2023-01-18 15:43:36 +0000

    Add files via upload

---
+++ front_end-swap.py
@@ -0,0 +1,127 @@
+from pytezos import pytezos
+from decouple import config
+import json
+import time
+import math
+
+priv_key = config('priv_key')
+
+pytezos = pytezos.using(shell='https://ghostnet.tezos.marigold.dev/', 
+                        key=priv_key)
+
+#Variable
+decimals = 10**18
+#Import contracts
+tokenx_address = 'KT1AR4CSxb15uFKacgKDGmPDSSvBLZ5m1Fz7'
+tokeny_address = 'KT1V6MLV1xN5yMhaF3jywm87Y4Vqi5fiPpxA'
+cfmm_address=config('cfmm_address')
+wallet_address='tz1eLA1kphjGGVP7iSABmEw5U7YChT88RZSW'
+
+def price_to_tick(p):
+    return math.floor(math.log(p, 1.0001))
+
+q80 = 2**80
+def price_to_sqrtp(p):
+    return int(math.sqrt(p) * q80)
+
+def liquidity0(amount, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return (amount * (pa * pb) / q80) / (pb - pa)
+
+def liquidity1(amount, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return amount * q80 / (pb - pa)
+
+def calc_amount0(liq, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return int(liq * q80 * (pb - pa) / pa / pb)
+
+
+def calc_amount1(liq, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return int(liq * (pb - pa) / q80)
+
+def calc_amount0(liq, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return int(liq * q80 * (pb - pa) / pa / pb)
+
+
+def calc_amount1(liq, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return int(liq * (pb - pa) / q80)
+
+#Swap x to y
+def swap_xtoy(cfmm_address):
+    cfmm = pytezos.contract(cfmm_address)
+    fee = 0.001
+    amount_in = 1*10**18
+    # sqrtp_cur = 2696296818671908502942364
+    # liq = 3388796696349066672112
+    sqrtp_cur = 2703324232521071249517405
+    liq = 2217758416937709731840
+
+    print(f"\nSelling {amount_in/10**18} X")
+
+    price_next = int((liq * q80 * sqrtp_cur) // (liq * q80 + amount_in * sqrtp_cur))
+
+    print("New price:", (price_next / q80) ** 2)
+    print("New sqrtP:", price_next)
+    print("New tick:", price_to_tick((price_next / q80) ** 2))
+
+    amount_in = calc_amount0(liq, price_next, sqrtp_cur)
+    amount_out = calc_amount1(liq, price_next, sqrtp_cur)
+
+    print("X in:", amount_in / 10**18)
+    print("Y out:", amount_out / 10**18)
+    print("In/Out=",amount_in/amount_out,'which should be bigger than the price')
+    xtoy = cfmm.x_to_y({
+            "deadline": 1704398681,
+            "dx": int(amount_in),
+            "min_dy": int(amount_out-amount_out*fee),
+            "to_dy": wallet_address
+    })
+    xtoy.send()
+    return xtoy
+
+#Swap y to x
+def swap_ytox(cfmm_address):
+    cfmm = pytezos.contract(cfmm_address)
+    fee = 0.001
+    amount_in = 5*10**18
+    # sqrtp_cur = 2696296818671908502942364
+    # liq = 3388796696349066672112
+    sqrtp_cur = 2703324232521071249517405
+    liq = 2217758416937709731840
+    price_diff = (amount_in * q80) / liq
+    price_next = sqrtp_cur + price_diff
+    print("New price:", (price_next / q80) ** 2)
+    print("New sqrtP:", price_next)
+    print("New tick:", price_to_tick((price_next / q80) ** 2))
+
+    amount_in = calc_amount1(liq, price_next, sqrtp_cur)
+    amount_out = calc_amount0(liq, price_next, sqrtp_cur)
+
+    print("Y in:", amount_in / 10**18)
+    print("X out:", amount_out / 10**18)
+    print("In/Out=",amount_in/amount_out, 'which should be bigger than 1/P')
+
+    ytox = cfmm.y_to_x({
+            "deadline": 1704398681,
+            "dy": int(amount_in),
+            "min_dx": int(amount_out*(1-fee)),
+            "to_dx": wallet_address
+    })
+
+    ytox.send()
+    return ytox
+
+# swap_xtoy(cfmm_address)
+# print('\n\n\n')
+
+swap_ytox(cfmm_address)
---
+++ front_end_pos.py
@@ -0,0 +1,76 @@
+from pytezos import pytezos
+from decouple import config
+import json
+import time
+import math
+
+priv_key = config('priv_key')
+
+pytezos = pytezos.using(shell='https://ghostnet.tezos.marigold.dev/', 
+                        key=priv_key)
+
+#Variable
+decimals = 10**18
+#Import contracts
+tokenx_address = 'KT1AR4CSxb15uFKacgKDGmPDSSvBLZ5m1Fz7'
+tokeny_address = 'KT1V6MLV1xN5yMhaF3jywm87Y4Vqi5fiPpxA'
+cfmm_address=config('cfmm_address')
+wallet_address='tz1eLA1kphjGGVP7iSABmEw5U7YChT88RZSW'
+
+def get_vals():
+    dy = 50*10**18
+    dx = 10*10**18
+
+    pu = 5.1
+    sqrt_pc = 2703324232521071249517405/2**80
+    pl = 4.9
+
+    iu = math.log(math.sqrt(pu),math.sqrt(1.0001))
+    ic = math.log(sqrt_pc,math.sqrt(1.0001))
+    il = math.log(math.sqrt(pl),math.sqrt(1.0001))
+
+    liq1 = dx*((math.sqrt(pu)*sqrt_pc)/(math.sqrt(pu)-sqrt_pc))
+    liq2 = dy/(sqrt_pc - math.sqrt(pl))
+    liq3 = dx*((math.sqrt(pu)*math.sqrt(1.0001**ic))/(math.sqrt(pu)-math.sqrt(1.0001**ic)))
+    liq4 = dy/(math.sqrt(1.0001**ic) - math.sqrt(pl))
+
+    liq = min(liq1,liq2, liq3, liq4)*0.9999
+
+    #THESE ARE TO BE SHOWN IN THE FRONTEND
+    # front_end_dx= liq*(math.sqrt(pu)-sqrt_pc)/(math.sqrt(pu)*sqrt_pc)
+    # front_end_dy= liq*(sqrt_pc - math.sqrt(pl))
+    
+    return round(iu), round(il), liq, dx, dy
+
+#Set Position
+def set_pos(cfmm_address, iu, il, liq, dx, dy):
+    cfmm = pytezos.contract(cfmm_address)
+    set_pos = cfmm.set_position({
+            "deadline": 1704398681,
+            "liquidity": int(liq),
+            "lower_tick_index": il,
+            "lower_tick_witness": -1048575,
+            "maximum_tokens_contributed": (int(dx), int(dy)),
+            "upper_tick_index": iu,
+            "upper_tick_witness": -1048575}
+    )
+
+    set_pos.send()
+    return set_pos
+
+'''
+@Dev: Beggin test
+
+'''
+print('\n --------------- \n Set Position Within Price Range:\n --------------- \n' )
+
+#Update operators to allow token usage by the contract
+# update_ops(tokenx_address, tokeny_address, cfmm_address)
+# time.sleep(20)
+
+#Get ticks and liquidity vals 
+(iu, il, liq, dx, dy)=get_vals()
+
+#Set position
+set_pos(cfmm_address, iu, il, liq, dx, dy)
+
