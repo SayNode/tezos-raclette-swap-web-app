dea739acea5b8088bde801d0fd5fc1d7589663a5
yann <yann@saynode.ch>
2023-08-15 14:46:37 +0200

    small fixes

--- tezos_swap_frontend/build/web/flutter_service_worker.js
+++ tezos_swap_frontend/build/web/flutter_service_worker.js
@@ -2,10 +2,11 @@
 const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
-const RESOURCES = {
-  "assets/AssetManifest.json": "c2f5812524541e5cd638383c5e1dedfc",
+
+const RESOURCES = {"assets/AssetManifest.bin": "6982055da031f1aa72849bf39f162b67",
+"assets/AssetManifest.json": "f9975a7e9210e2411bdd2ae53aef5624",
 "assets/assets/data/contracts.json": "da8692e68c2c5d61e78aceef59eda1f2",
-"assets/assets/data/token.json": "357be937c5c4287df414834f4e3ac9af",
+"assets/assets/data/token.json": "161f26033a0ea9c838de2cbf36e70a94",
 "assets/assets/image/BG%2520(0).jpg": "c4431049959227b889b535d95c8e21e3",
 "assets/assets/image/BG%2520(1).jpg": "57a15be9991bfc77bbac61db384755b8",
 "assets/assets/image/BG%2520(10).jpg": "5a41f3453bea23cff079ece2e61c29fe",
@@ -31,36 +32,64 @@ const RESOURCES = {
 "assets/assets/image/Tezos_Logo_2022.png": "9499621c14f07205d264c420f895d76c",
 "assets/assets/translations/de.json": "6e23c7bd4b485627ba319067aeab249a",
 "assets/assets/translations/en.json": "37b8e6058195a2296689d16da3b14a09",
+"assets/data/contracts.json": "da8692e68c2c5d61e78aceef59eda1f2",
+"assets/data/token.json": "357be937c5c4287df414834f4e3ac9af",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "01b8322a186b8a14170c822ad1f1bf41",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/shaders/ink_sparkle.frag": "ea069b3696bd7d3013e627bbf48994ec",
-"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
-"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
-"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
+"assets/fonts/MaterialIcons-Regular.otf": "b9ebf29ebcc7b6749cdb4da49b781bd2",
+"assets/image/BG%20(0).jpg": "c4431049959227b889b535d95c8e21e3",
+"assets/image/BG%20(1).jpg": "57a15be9991bfc77bbac61db384755b8",
+"assets/image/BG%20(10).jpg": "5a41f3453bea23cff079ece2e61c29fe",
+"assets/image/BG%20(11).jpg": "28fc0fa9ad114dab6bba14a2cf774a69",
+"assets/image/BG%20(12).jpg": "961f6fe43083326687e4b73372d6d35c",
+"assets/image/BG%20(13).jpg": "ead5a5cd897736bcf716ab37092e787c",
+"assets/image/BG%20(2).jpg": "d4ee440d77c3669f9463410f69df2b10",
+"assets/image/BG%20(3).jpg": "1c33801ea880d54f10ae6b54f0a30d20",
+"assets/image/BG%20(4).jpg": "fecef67abc5c4cac6b8b209551550683",
+"assets/image/BG%20(5).jpg": "b6e5954e39a4d79c5e16f64bacdab3cc",
+"assets/image/BG%20(6).jpg": "714945aa60573c8f0e279092cfcc40e5",
+"assets/image/BG%20(7).jpg": "167da6b5c0dda967a9bfcf6780c9b583",
+"assets/image/BG%20(8).jpg": "489a735d3f291bc8e38a345a23f0fb17",
+"assets/image/BG%20(9).jpg": "2b19b3e24b69e88c6820825463a0d908",
+"assets/image/bsc_logo.png": "072596a52321c716a03ed8f8c0889a0d",
+"assets/image/eth_logo.png": "d400b3be0533e6f038c273ceaba5ebd5",
+"assets/image/eth_logo_black.png": "3976d1fb52c83b5a643c801612fd6fe8",
+"assets/image/Logo.png": "b4a4a898190e6137448da75ab5e6eb16",
+"assets/image/logo_1.png": "a3e669267deacb1cdaaf9d6cba6fba3a",
+"assets/image/logo_medium.png": "2b3c6bc39956d51e0cee5ff70d5a08c1",
+"assets/image/logo_medium_1.png": "677a0490e47a6fbd0f553c0a81f35e22",
+"assets/image/Logo_w.png": "eccd640fa5a318a5681fc198c27c1ccf",
+"assets/image/Tezos_Logo_2022.png": "9499621c14f07205d264c420f895d76c",
+"assets/NOTICES": "476d1fb974530eeb62bc085d3c446544",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
+"assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
+"canvaskit/canvaskit.js": "76f7d822f42397160c5dfc69cbc9b2de",
+"canvaskit/canvaskit.wasm": "f48eaf57cada79163ec6dec7929486ea",
+"canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b5652e05",
+"canvaskit/chromium/canvaskit.wasm": "fc18c3010856029414b70cae1afc5cd9",
+"canvaskit/profiling/canvaskit.js": "e069e181424052299c5bffb1b300bb13",
 "canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
+"canvaskit/skwasm.js": "1df4d741f441fa1a4d10530ced463ef8",
+"canvaskit/skwasm.wasm": "6711032e17bf49924b2b001cef0d3ea3",
+"canvaskit/skwasm.worker.js": "19659053a277272607529ef87acf9d8a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
+"flutter.js": "6b515e434cea20006b3ef1726d2c8894",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "6b1d110537929d24f9427aaa757565ff",
-"/": "6b1d110537929d24f9427aaa757565ff",
-"main.dart.js": "62f01a57e12de6bbe0f81852ca3d618a",
+"index.html": "d498843f71cbafc9e8322cd894f4697b",
+"/": "d498843f71cbafc9e8322cd894f4697b",
+"main.dart.js": "24b9a2991df7a6b015895484ef3545fe",
 "manifest.json": "e78302696d1b47a98d35c18570d45684",
 "sodium.js": "4652c40c29e6140369a6deca42ebb770",
-"version.json": "813a653185f1a7e92235b092db779f98"
-};
-
+"version.json": "813a653185f1a7e92235b092db779f98"};
 // The application shell files that are downloaded before a service worker can
 // start.
-const CORE = [
-  "main.dart.js",
+const CORE = ["main.dart.js",
 "index.html",
 "assets/AssetManifest.json",
 "assets/FontManifest.json"];
+
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
   self.skipWaiting();
@@ -71,7 +100,6 @@ self.addEventListener("install", (event) => {
     })
   );
 });
-
 // During activate, the cache is populated with the temp files downloaded in
 // install. If this service worker is upgrading from one with a saved
 // MANIFEST, then use this to retain unchanged resource files.
@@ -93,6 +121,8 @@ self.addEventListener("activate", function(event) {
         await caches.delete(TEMP);
         // Save the manifest to make future upgrades efficient.
         await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
         return;
       }
       var oldManifest = await manifest.json();
@@ -118,6 +148,8 @@ self.addEventListener("activate", function(event) {
       await caches.delete(TEMP);
       // Save the manifest to make future upgrades efficient.
       await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
       return;
     } catch (err) {
       // On an unhandled exception the state of the cache cannot be guaranteed.
@@ -128,7 +160,6 @@ self.addEventListener("activate", function(event) {
     }
   }());
 });
-
 // The fetch handler redirects requests for RESOURCE files to the service
 // worker cache.
 self.addEventListener("fetch", (event) => {
@@ -168,7 +199,6 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
-
 self.addEventListener('message', (event) => {
   // SkipWaiting can be used to immediately activate a waiting service worker.
   // This will also require a page refresh triggered by the main worker.
@@ -181,7 +211,6 @@ self.addEventListener('message', (event) => {
     return;
   }
 });
-
 // Download offline will check the RESOURCES for all files not in the cache
 // and populate them.
 async function downloadOffline() {
@@ -202,7 +231,6 @@ async function downloadOffline() {
   }
   return contentCache.addAll(resources);
 }
-
 // Attempt to download the resource online before falling back to
 // the offline cache.
 function onlineFirst(event) {
--- tezos_swap_frontend/lib/services/wallet_connection.dart
+++ tezos_swap_frontend/lib/services/wallet_connection.dart
@@ -1,13 +1,15 @@
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
 import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:nanoid/nanoid.dart';
 import 'package:tezos_swap_frontend/pages/widgets/card_route.dart';
 import 'package:tezos_swap_frontend/utils/globals.dart' as global;
 import 'package:tezos_swap_frontend/utils/utils.dart';
-import '../pages/widgets/card_route_not dismissable.dart';
+
 import '../theme/ThemeRaclette.dart';
 
 class WalletService extends GetxService {
@@ -15,15 +17,12 @@ class WalletService extends GetxService {
   RxBool connected = false.obs;
 
   authorize(String tokenContract, String swapContract) async {
-    bool isOp =
-        await checkIfOperator(tokenContract, address.value, swapContract);
+    bool isOp = await checkIfOperator(tokenContract, address.value, swapContract);
     if (!isOp) {
       String id = nanoid();
       html.window.addEventListener("message", (event) {
         final evt = (event as html.MessageEvent);
-        if (evt.source == html.window &&
-            evt.data['reqId'] == id &&
-            evt.data['type'] == 'TEMPLE_PAGE_RESPONSE') {
+        if (evt.source == html.window && evt.data['reqId'] == id && evt.data['type'] == 'TEMPLE_PAGE_RESPONSE') {
           Get.close(1);
         }
       }, true);
@@ -38,9 +37,7 @@ class WalletService extends GetxService {
                 height: 500,
                 child: Card(
                   color: ThemeRaclette.primaryStatic,
-                  shape: RoundedRectangleBorder(
-                      side: const BorderSide(color: Colors.white, width: 2.0),
-                      borderRadius: BorderRadius.circular(20.0)),
+                  shape: RoundedRectangleBorder(side: const BorderSide(color: Colors.white, width: 2.0), borderRadius: BorderRadius.circular(20.0)),
                   child: const Center(
                     child: Text('Waiting for authorization.'),
                   ),
@@ -57,15 +54,15 @@ class WalletService extends GetxService {
     _request({
       "type": "PERMISSION_REQUEST",
       "network": global.network,
-      "appMeta": {"name": "TezosSwap"},
+      "appMeta": {
+        "name": "TezosSwap"
+      },
       "force": true
     });
   }
 
 //FIXME: might be problems with tokenx and y amount calculation
-  swap(String contract, String recipient, double tokenX, double tokenY,
-      String tokenXAddress, String tokenYAddress,
-      {bool yToX = false}) async {
+  swap(String contract, String recipient, double tokenX, double tokenY, String tokenXAddress, String tokenYAddress, {bool yToX = false}) async {
     String entrypoint = "x_to_y";
     if (yToX) {
       entrypoint = "y_to_x";
@@ -91,15 +88,23 @@ class WalletService extends GetxService {
                 {
                   "prim": "Pair",
                   "args": [
-                    {"string": "2023-09-20T10:19:24Z"}, //TODO: set date
-                    {"int": "$x"}
+                    {
+                      "string": "2023-09-20T10:19:24Z"
+                    }, //TODO: set date
+                    {
+                      "int": "$x"
+                    }
                   ]
                 },
                 {
                   "prim": "Pair",
                   "args": [
-                    {"int": "$y"},
-                    {"string": recipient}
+                    {
+                      "int": "$y"
+                    },
+                    {
+                      "string": recipient
+                    }
                   ]
                 }
               ]
@@ -136,10 +141,7 @@ class WalletService extends GetxService {
 
     var currentTick = await getCurrentTick(contract);
 
-    BigInt liquidity = etherToWei(
-        await getLiquidity(
-            yDouble, xDouble, lowerPrice, upperPrice, currentTick),
-        18);
+    BigInt liquidity = etherToWei(await getLiquidity(yDouble, xDouble, lowerPrice, upperPrice, currentTick), 18);
 
     BigInt x = etherToWei(xDouble, 18);
     BigInt y = etherToWei(yDouble, 18);
@@ -163,15 +165,23 @@ class WalletService extends GetxService {
                     {
                       "prim": "Pair",
                       "args": [
-                        {"int": "2665581460"}, //TODO: set deadline
-                        {"int": "$liquidity"}
+                        {
+                          "int": "2665581460"
+                        }, //TODO: set deadline
+                        {
+                          "int": "$liquidity"
+                        }
                       ]
                     },
                     {
                       "prim": "Pair",
                       "args": [
-                        {"int": "${lowerTick.toInt()}"},
-                        {"int": '-1048575'}
+                        {
+                          "int": "${lowerTick.toInt()}"
+                        },
+                        {
+                          "int": '-1048575'
+                        }
                       ]
                     }
                   ]
@@ -185,14 +195,22 @@ class WalletService extends GetxService {
                         {
                           "prim": "Pair",
                           "args": [
-                            {"int": "$x"},
-                            {"int": "$y"}
+                            {
+                              "int": "$x"
+                            },
+                            {
+                              "int": "$y"
+                            }
                           ]
                         },
-                        {"int": "${upperTick.toInt()}"}
+                        {
+                          "int": "${upperTick.toInt()}"
+                        }
                       ]
                     },
-                    {"int": "${upperWitness.last}"}
+                    {
+                      "int": "${upperWitness.last}"
+                    }
                   ]
                 }
               ]
@@ -203,8 +221,7 @@ class WalletService extends GetxService {
     });
   }
 
-  authorizeContract(
-      String tokenContract, String swapContract, String id) async {
+  authorizeContract(String tokenContract, String swapContract, String id) async {
     _request({
       "type": "OPERATION_REQUEST",
       "sourcePkh": address.value,
@@ -223,12 +240,18 @@ class WalletService extends GetxService {
                   {
                     "prim": "Pair",
                     "args": [
-                      {"string": address.value},
+                      {
+                        "string": address.value
+                      },
                       {
                         "prim": "Pair",
                         "args": [
-                          {"string": swapContract},
-                          {"int": "0"}
+                          {
+                            "string": swapContract
+                          },
+                          {
+                            "int": "0"
+                          }
                         ]
                       }
                     ]
@@ -267,8 +290,12 @@ class WalletService extends GetxService {
                       "prim": "Pair",
                       "args": [
                         //TODO: do proper deadline
-                        {"int": "2668508020"},
-                        {"int": liquidity}
+                        {
+                          "int": "2668508020"
+                        },
+                        {
+                          "int": liquidity
+                        }
                       ]
                     },
                     {
@@ -277,11 +304,17 @@ class WalletService extends GetxService {
                         {
                           "prim": "Pair",
                           "args": [
-                            {"int": "0"},
-                            {"int": "0"}
+                            {
+                              "int": "0"
+                            },
+                            {
+                              "int": "0"
+                            }
                           ]
                         },
-                        {"int": position['key']}
+                        {
+                          "int": position['key']
+                        }
                       ]
                     }
                   ]
@@ -289,8 +322,12 @@ class WalletService extends GetxService {
                 {
                   "prim": "Pair",
                   "args": [
-                    {"string": address.string},
-                    {"string": address.string}
+                    {
+                      "string": address.string
+                    },
+                    {
+                      "string": address.string
+                    }
                   ]
                 }
               ]
@@ -312,10 +349,8 @@ class WalletService extends GetxService {
               height: 500,
               child: Card(
                 color: ThemeRaclette.primaryStatic,
-                shape: RoundedRectangleBorder(
-                    side: const BorderSide(color: Colors.white, width: 2.0),
-                    borderRadius: BorderRadius.circular(20.0)),
-                child: Center(
+                shape: RoundedRectangleBorder(side: const BorderSide(color: Colors.white, width: 2.0), borderRadius: BorderRadius.circular(20.0)),
+                child: const Center(
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
@@ -333,15 +368,17 @@ class WalletService extends GetxService {
 
   _request(Map payload, {String? id}) {
     id ??= nanoid();
-    var msg = {'type': 'TEMPLE_PAGE_REQUEST', 'payload': payload, 'reqId': id};
+    var msg = {
+      'type': 'TEMPLE_PAGE_REQUEST',
+      'payload': payload,
+      'reqId': id
+    };
     _signPopup();
     html.window.postMessage(msg, "*");
 
     html.window.addEventListener("message", (event) {
       final evt = (event as html.MessageEvent);
-      if (evt.source == html.window &&
-          evt.data['reqId'] == id &&
-          evt.data['type'] == 'TEMPLE_PAGE_RESPONSE') {
+      if (evt.source == html.window && evt.data['reqId'] == id && evt.data['type'] == 'TEMPLE_PAGE_RESPONSE') {
         if (evt.data['payload']['type'] == 'PERMISSION_RESPONSE') {
           Get.close(1);
           address.value = evt.data['payload']['pkh'];
@@ -354,13 +391,10 @@ class WalletService extends GetxService {
                   padding: const EdgeInsets.all(8.0),
                   child: SizedBox(
                     width: 400,
-                    height: 500,
+                    //height: 500,
                     child: Card(
                       color: ThemeRaclette.primaryStatic,
-                      shape: RoundedRectangleBorder(
-                          side:
-                              const BorderSide(color: Colors.white, width: 2.0),
-                          borderRadius: BorderRadius.circular(20.0)),
+                      shape: RoundedRectangleBorder(side: const BorderSide(color: Colors.white, width: 2.0), borderRadius: BorderRadius.circular(20.0)),
                       child: Center(
                         child: Padding(
                           padding: const EdgeInsets.all(16.0),
@@ -368,7 +402,17 @@ class WalletService extends GetxService {
                             mainAxisSize: MainAxisSize.min,
                             children: [
                               const Text('Operation signed. Operation-hash:'),
-                              Text(evt.data['payload']['opHash']),
+                              Row(
+                                children: [
+                                  Text(shortenHex(evt.data['payload']['opHash'])),
+                                  GestureDetector(
+                                    onTap: () => Clipboard.setData(ClipboardData(text: evt.data['payload']['opHash'])).then((_) {
+                                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Hash has been copied to clipboard")));
+                                    }),
+                                    child: const Icon(Icons.copy),
+                                  )
+                                ],
+                              ),
                             ],
                           ),
                         ),
@@ -380,9 +424,7 @@ class WalletService extends GetxService {
             },
           ));
         }
-      } else if (evt.source == html.window &&
-          evt.data['reqId'] == id &&
-          evt.data['type'] == 'TEMPLE_PAGE_ERROR_RESPONSE') {
+      } else if (evt.source == html.window && evt.data['reqId'] == id && evt.data['type'] == 'TEMPLE_PAGE_ERROR_RESPONSE') {
         Navigator.of(Get.context!).pushReplacement(CardDialogRoute(
           builder: (context) {
             return Center(
@@ -393,9 +435,7 @@ class WalletService extends GetxService {
                   height: 500,
                   child: Card(
                     color: ThemeRaclette.primaryStatic,
-                    shape: RoundedRectangleBorder(
-                        side: const BorderSide(color: Colors.white, width: 2.0),
-                        borderRadius: BorderRadius.circular(20.0)),
+                    shape: RoundedRectangleBorder(side: const BorderSide(color: Colors.white, width: 2.0), borderRadius: BorderRadius.circular(20.0)),
                     child: Center(
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
@@ -413,4 +453,12 @@ class WalletService extends GetxService {
       }
     }, true);
   }
+}
+
+//mehod to shorten hex strings
+String shortenHex(String hex) {
+  if (hex.length < 10) {
+    return hex;
+  }
+  return '${hex.substring(0, 6)}...${hex.substring(hex.length - 4)}';
 }
